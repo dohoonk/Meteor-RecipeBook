039997e3d9587de81fc79bf86a9c2ec328029b69
Dohoon Kim <dohoonk.smg@gmail.com>
2016-06-06 02:12:12 -0700

    Add Update Method For Menu

--- collections/Recipes.js
+++ collections/Recipes.js
@@ -3,6 +3,9 @@ Recipes = new Mongo.Collection('recipes');
 Recipes.allow({
   insert: function(userId, doc) {
     return !!userId;
+  },
+  update: function(userId, doc) {
+    return !!userId;
   }
 });
 
@@ -27,6 +30,14 @@ RecipeSchema = new SimpleSchema({
   ingredients: {
     type: [Ingredient]
   },
+  inMenu: {
+    type: Boolean,
+    defaultValue: false,
+    optional: true,
+    autoform: {
+      type: "hidden"
+    }
+  },
   author: {
     type: String,
     label: "Author",
@@ -46,6 +57,16 @@ RecipeSchema = new SimpleSchema({
     autoform: {
       type: "hidden"
     }
+  }
+});
+
+Meteor.methods({
+  toggleMenuItem: function(id, currentState) {
+    Recipes.update(id, {
+      $set: {
+        inMenu: !currentState
+      }
+    });
   }
 });
 
